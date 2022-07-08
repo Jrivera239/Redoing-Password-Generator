bfde491346c201d39e20c22640b9b036484625c9
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-08 14:00:11 -0400

    java elements

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
 
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "~!@#$%^&*()_+=-";
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase1 = "abcdefghijklmnopqrstuvwxyz";
+const numbers1 = "0123456789";
+const symbols1 = "~!@#$%^&*()_+=-";
+const uppercase1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const upperCase = doument.getElementById("uppercase");
+const lowerCase = doument.getElementById("lowercase");
+const Numbers = doument.getElementById("numbers");
+const Symbols = doument.getElementById("symbols");
+const Generate = doument.getElementById("generatebtn");
+const password = doument.getElementById("pw");
+
+
+submit.addEventListener("click",() =>{
+    let initialPassword = empty;
+    (upperCase,checked) ? initialPassword +=uppercase1 : "";
+    (lowerCase,checked) ? initialPassword +=lowercase1 : "";
+    (Symbols,checked) ? initialPassword +=symbols1 : "";
+    (Numbers,checked) ? initialPassword +=numbers1 : "";
+
+
+});
