eb40631af79a4496f03de72401eaed68c37807b2
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-08 14:30:49 -0400

    fixing issues

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,32 @@
-
+// Elements and IDs//
 const lowercase1 = "abcdefghijklmnopqrstuvwxyz";
 const numbers1 = "0123456789";
 const symbols1 = "~!@#$%^&*()_+=-";
 const uppercase1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const Length = document.getElementById("length")
+const upperCase = document.getElementById("uppercase");
+const lowerCase = document.getElementById("lowercase");
+const Numbers = document.getElementById("numbers");
+const Symbols = document.getElementById("symbols");
+const Generate = document.getElementById("generatebtn");
+const password = document.getElementById("pw");
+const empty = "";
 
-const upperCase = doument.getElementById("uppercase");
-const lowerCase = doument.getElementById("lowercase");
-const Numbers = doument.getElementById("numbers");
-const Symbols = doument.getElementById("symbols");
-const Generate = doument.getElementById("generatebtn");
-const password = doument.getElementById("pw");
-
-
-submit.addEventListener("click",() =>{
+//connecting checkbox function//
+generatebtn.addEventListener("click",() => {
     let initialPassword = empty;
-    (upperCase,checked) ? initialPassword +=uppercase1 : "";
-    (lowerCase,checked) ? initialPassword +=lowercase1 : "";
-    (Symbols,checked) ? initialPassword +=symbols1 : "";
-    (Numbers,checked) ? initialPassword +=numbers1 : "";
+    (upperCase.checked) ? initialPassword +=uppercase1 :"";
+    (lowerCase.checked) ? initialPassword +=lowercase1 :"";
+    (Symbols.checked) ? initialPassword +=symbols1 :"";
+    (Numbers.checked) ? initialPassword +=numbers1 :"";
 
-
+    password.value = generatePassword(Length.value,initialPassword)
 });
+//loop through initial password//
+function generatePassword(length, initialPassword){
+    let pass = ""
+    for(let i = 0; i < length; i ++) {
+        pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
+    }
+    return pass;
+}
